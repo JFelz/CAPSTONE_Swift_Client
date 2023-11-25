6f098f1feb9a3da1eea79e154170338eecae2cce
Jovanni Feliz <feliz.jovanni@gmail.com>
2023-11-25 13:48:20 -0600

    Client cReadUD is done

--- components/client/ClientProductCard.js
+++ components/client/ClientProductCard.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  CardMedia, Typography, CardContent, Rating, CardActionArea,
+} from '@mui/material';
 
-export default function ClientProductCard() {
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+
+export default function ClientProductCard({ prodObj }) {
+  const [value, setValue] = useState();
   return (
     <>
+      <Link href={`/client/shop/${prodObj.id}`} passHref>
+        <CardActionArea sx={{
+          width: 220, margin: '5px', boxShadow: '0px 0px 0px 0px', borderRadius: '0px', backgroundColor: '#F2F2F2',
+        }}
+        >
+          <CardMedia
+            sx={{ height: 300 }}
+            image={prodObj.imageUrl1}
+            title="green iguana"
+          />
+          <CardContent>
+            <Rating
+              name="simple-controlled"
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+            />
+            <Typography gutterBottom component="div" style={{ fontSize: '.85em' }}>
+              {prodObj.title}
+            </Typography>
+            <Typography gutterBottom component="div" variant="body2" color="text.secondary" style={{ fontSize: '1em' }}>
+              ${prodObj.price}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
     </>
   );
 }
+
+ClientProductCard.propTypes = {
+  prodObj: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    category: PropTypes.string,
+    price: PropTypes.number,
+    length: PropTypes.number,
+    width: PropTypes.number,
+    wheelbase: PropTypes.number,
+    skateSpots: PropTypes.string,
+    imageUrl1: PropTypes.string,
+    imageUrl2: PropTypes.string,
+    imageUrl3: PropTypes.string,
+  }).isRequired,
+};
---
+++ pages/client/shop/[id].js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Button, Image, Container } from 'react-bootstrap';
+import { getSingleProducts } from '../../../api/productData';
+
+export default function ClientProductViewPage() {
+  const [product, setProduct] = useState();
+  const router = useRouter();
+  const { id } = router.query;
+
+  const currentProduct = () => {
+    getSingleProducts(id).then(setProduct);
+  };
+
+  useEffect(() => {
+    currentProduct();
+  }, []);
+
+  return (
+    <>
+      <section className="viewProduct-top-section">
+        <div>
+          <Link href="/client/shop/main" passHref>
+            <Button variant="secondary"> Go Back </Button>
+          </Link>
+        </div>
+      </section>
+      <section className="viewProduct-mid-section">
+        <div className="productSplit">
+          <div className="LeftSideProductPage">
+            <Image
+              src={product?.imageUrl1}
+              alt="Product Image"
+              fluid
+            />
+
+            {/* Main image is a displayer. You have a row of buttons on the bottom. The productObj is already pulling in the images and storing it in state. On onClick command, you display X image on the main image display component.
+
+            For an image display, you would store one image in state. The button updates that state with the new image you click.
+
+            Look at LandYachtz and see how they do their sizing for the right section of their page. #Devtools
+            */}
+
+            {/* <div className="BottomRowImages">
+              <Image
+                src={product?.imageUrl2}
+                alt="Product Image"
+                width={100}
+                height={110}
+              />
+            </div> */}
+          </div>
+          <Container className="RightSideProductPage">
+            Right Side
+            <Container className="RightContainer">
+              <h1>{product?.title}</h1>
+              <p> {product?.description} </p>
+              <div>
+                <h4> Measurements </h4>
+                <p>Length: {product?.length}</p>
+                <p>Width: {product?.width}</p>
+                <p>Wheelbase: {product?.wheelbase}</p>
+                <h4> Skate Spots </h4>
+                <p>{product?.skateSpots}</p>
+              </div>
+              <p> Price: ${product?.price} </p>
+              <section className="ViewPageCartSection">
+                <Button className="CartButton">
+                  Add To Cart
+                </Button>
+              </section>
+            </Container>
+          </Container>
+        </div>
+      </section>
+      <section className="viewProduct-bot-section">
+        <div>
+          Comment Section
+        </div>
+      </section>
+    </>
+  );
+}
