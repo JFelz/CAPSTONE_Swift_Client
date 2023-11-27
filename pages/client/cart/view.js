c7375f05835e5c0e8242ed4cc72f6bbcff45d3cc
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-11-27 16:15:54 -0600

    Designing the form

--- pages/client/cart/view.js
+++ pages/client/cart/view.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import {
-  Button, Card, CardActions, CardContent,
+  Button, Card, CardActions, CardContent, Typography,
 } from '@mui/material';
 import Link from 'next/link';
+import { Form } from 'react-bootstrap';
 import { useAuth } from '../../../utils/context/authContext';
 import { getCartUserUID } from '../../../api/cartData';
 import CartProductCard from '../../../components/client/CartProductCard';
@@ -35,7 +36,66 @@ export default function Cart() {
           {currentProduct[0]?.map((prod) => <CartProductCard key={prod.id} productObj={prod} />)}
         </CardContent>
         <CardContent className="RightSideCartPage">
-          <div> 50 </div>
+          <Form>
+            <Form.Label> BILLING INFORMATION </Form.Label>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label> PERSONAL DETAILS </Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Full Name"
+              />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlInput1" className="SplitInputFields">
+              <Form.Control
+                type="email"
+                placeholder="Email Address"
+              />
+              <Form.Control
+                type="number"
+                placeholder="Phone Number"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>COUNTRY</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Street Address"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>STREET ADDRESS</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Street Address"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>TOWN/CITY</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Street Address"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>STATE</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Street Address"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>ZIP CODE</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Street Address"
+              />
+            </Form.Group>
+          </Form>
+          <CardContent>
+            <Typography>
+              Total
+            </Typography>
+          </CardContent>
           <CardActions>
             <Button size="medium" variant="contained" className="PlaceOrderBtn">Place Order</Button>
           </CardActions>
